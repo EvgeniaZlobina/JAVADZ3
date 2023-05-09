fca865e70580a9bd080da69755fb59d21ccf6e41
Evgeniy Zlobina <ZEA_6590@mail.ru>
2023-05-09 09:04:45 +0300

    2 commit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,10 @@
+public class BonusMilesService {
+        public int calculate(int price)
+        {
+            int res;
+            res = price / 20;
+            return res;
+        }
+
+    }
+
