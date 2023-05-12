20ff09fb202f92eb12ee5c3b5aa3abc7ba9ebee2
Evgeniy Zlobina <ZEA_6590@mail.ru>
2023-05-12 22:31:01 +0300

    Done reformat code

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,10 +1,8 @@
 public class BonusMilesService {
-        public int calculate(int price)
-        {
-            int res;
-            res = price / 20;
-            return res;
-        }
-
+    public int calculate(int price) {
+        int res;
+        res = price / 20;
+        return res;
     }
+}
 
